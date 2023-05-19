1ce6ebf2524173c4e134d013f6eda965b49691d6
Saket Makhija - s0m0oi9 <sktmakhija@gmail.com>
2023-05-19 16:32:55 +0530

    inception

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("div",
+{id: "parent"},
+React.createElement("div",
+{id: "child"},
+React.createElement("h1",{},"Hello world")));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
