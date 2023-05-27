d1ce6635c4fc3aea9f655a14225810a796fc39c1
Saket Makhija - s0m0oi9 <sktmakhija@gmail.com>
2023-05-27 22:20:28 +0530

    add gitignore

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement("div",
 {id: "parent"},
 React.createElement("div",
 {id: "child"},
-React.createElement("h1",{},"Hello world")));
+React.createElement("h1",{},"Hello world from saket")));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
